6ccd4d31790a9757433b85c2a8bc00fc8dd2087a
Arthur Kern <79586960+oarthurkern@users.noreply.github.com>
2021-08-17 22:11:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+function saibamais () {
+
+const pontos = document.getElementById("pontos");
+const mais = document.getElementById("mais");
+const botao = document.getElementById("button");
+
+if (pontos.style.display === "none") {
+        pontos.style.display = "inline";
+        mais.style.display = "none";
+        botao.innerHTML= "SAIBA MAIS";
+    } else {
+        pontos.style.display = "none";
+        mais.style.display = "inline";
+        botao.innerHTML= "SAIBA MENOS";
+    }
+}
+function saibamais2 () {
+
+    const pontos = document.getElementById("pontos2");
+    const mais = document.getElementById("mais2");
+    const botao = document.getElementById("button2");
+    
+    if (pontos.style.display === "none") {
+            pontos.style.display = "inline";
+            mais.style.display = "none";
+            botao.innerHTML= "SAIBA MAIS";
+        } else {
+            pontos.style.display = "none";
+            mais.style.display = "inline";
+            botao.innerHTML= "SAIBA MENOS";
+        }
+}
+function saibamais3 () {
+
+    const pontos = document.getElementById("pontos3");
+    const mais = document.getElementById("mais3");
+    const botao = document.getElementById("button3");
+    
+    if (pontos.style.display === "none") {
+            pontos.style.display = "inline";
+            mais.style.display = "none";
+            botao.innerHTML= "SAIBA MAIS";
+        } else {
+            pontos.style.display = "none";
+            mais.style.display = "inline";
+            botao.innerHTML= "SAIBA MENOS";
+        }
+}
+function saibamais4 () {
+
+    const pontos = document.getElementById("pontos4");
+    const mais = document.getElementById("mais4");
+    const botao = document.getElementById("button4");
+    
+    if (pontos.style.display === "none") {
+            pontos.style.display = "inline";
+            mais.style.display = "none";
+            botao.innerHTML= "SAIBA MAIS";
+        } else {
+            pontos.style.display = "none";
+            mais.style.display = "inline";
+            botao.innerHTML= "SAIBA MENOS";
+        }
+}
+function saibamais5 () {
+
+    const pontos = document.getElementById("pontos5");
+    const mais = document.getElementById("mais5");
+    const botao = document.getElementById("button5");
+    
+    if (pontos.style.display === "none") {
+            pontos.style.display = "inline";
+            mais.style.display = "none";
+            botao.innerHTML= "SAIBA MAIS";
+        } else {
+            pontos.style.display = "none";
+            mais.style.display = "inline";
+            botao.innerHTML= "SAIBA MENOS";
+        }
+}
+
+
